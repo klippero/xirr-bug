9c5704757fcbeac2a9aed40dec8fff29672fda79
Santi <s@alvarezrojo.es>
2024-06-16 12:54:17 +0200

    16

---
+++ main.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require 'date'
+require "xirr"
+
+
+class Test < Minitest::Test
+  def test_bug
+    cf = Xirr::Cashflow.new
+
+    cf << Xirr::Transaction.new(-400,date:Date.strptime("10/09/2019","%d/%m/%Y"))
+    cf << Xirr::Transaction.new(-400,date:Date.strptime("21/01/2020","%d/%m/%Y"))
+    cf << Xirr::Transaction.new(-400,date:Date.strptime("24/02/2020","%d/%m/%Y"))
+    cf << Xirr::Transaction.new(-895.24,date:Date.strptime("16/03/2020","%d/%m/%Y"))
+
+    assert_equal 37.34, (cf.xirr * 100).round(2)
+  end
+end
