05c8765c31ec91d659d5025f991b05de4637c1fd
Santiago Álvarez <s@alvarezrojo.es>
2024-06-16 13:02:36 +0200

    16

--- Gemfile
+++ Gemfile
@@ -1,10 +1,7 @@
 source "https://rubygems.org"
 
-ruby "3.3.2"
-
 gem "minitest"
 gem 'date'
-gem 'gnucash'
 
 gem "xirr", "= 0.7.1", git: "https://github.com/djromero/xirr.git"
 gem "activesupport", require: "active_support/all"
--- main.rb
+++ main.rb
@@ -10,8 +10,8 @@ class Test < Minitest::Test
     cf << Xirr::Transaction.new(-400,date:Date.strptime("10/09/2019","%d/%m/%Y"))
     cf << Xirr::Transaction.new(-400,date:Date.strptime("21/01/2020","%d/%m/%Y"))
     cf << Xirr::Transaction.new(-400,date:Date.strptime("24/02/2020","%d/%m/%Y"))
-    cf << Xirr::Transaction.new(-895.24,date:Date.strptime("16/03/2020","%d/%m/%Y"))
+    cf << Xirr::Transaction.new(895.24,date:Date.strptime("16/03/2020","%d/%m/%Y"))
 
-    assert_equal 37.34, (cf.xirr * 100).round(2)
+    assert_equal( -74.20, (cf.xirr * 100).round(2))
   end
 end
